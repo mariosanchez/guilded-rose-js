63375c612b015f3cfae40c99104fb66abebbd5ce
Mario Sánchez <mr.mario.sanchez@gmail.com>
2021-04-23 12:40:54 +0200

    adds new case for backstage passde sell in

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,18 +1,20 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("+5 Dexterity Vest", 10, 20), // should decrease sellIn and Quality by 1 for regular items
+  new Item("Elixir of the Mongoose", 0, 7), // should decrease sellIn and Quality by 1 for regular items
+  new Item("Aged Brie", 2, 0), // aged brie should increase its quality when sellIn decreases
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80), 
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50), // backstage passes Quality drops to 0 when sellIn is passed
 
   // This Conjured item does not work properly yet
   new Item("Conjured Mana Cake", 3, 6),
 ];
+
 describe("Gilded Rose", function() {
   it("should work as expected", () => {
     const  gildedRose = new Shop(items);
@@ -21,13 +23,14 @@ describe("Gilded Rose", function() {
 
     const expectedUpdatedItems = [
       new Item("+5 Dexterity Vest", 9, 19),
+      new Item("Elixir of the Mongoose", -1, 5),
       new Item("Aged Brie", 1, 1),
-      new Item("Elixir of the Mongoose", 4, 6),
       new Item("Sulfuras, Hand of Ragnaros", 0, 80),
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
       new Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
       new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0), 
 
       // This Conjured item does not work properly yet
       new Item("Conjured Mana Cake", 2, 5),
